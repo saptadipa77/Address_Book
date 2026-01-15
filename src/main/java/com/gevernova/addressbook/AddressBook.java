93d07c3ee1640a234836aa9e0a6912b6e2ab283c
saptadipa77 <saptadipadatta5@gmail.com>
2026-01-15 11:51:42 +0530

    add 12th uc

--- src/main/java/com/gevernova/addressbook/AddressBook.java
+++ src/main/java/com/gevernova/addressbook/AddressBook.java
@@ -31,6 +31,9 @@ public class AddressBook {
     public void sortEntries(int choice) {
         Comparator<Contact> comp = switch (choice) {
             case 1 -> Comparator.comparing(Contact::getFirstName);
+            case 2 -> Comparator.comparing(Contact::getCity);      // UC 12
+            case 3 -> Comparator.comparing(Contact::getState);     // UC 12
+            case 4 -> Comparator.comparing(Contact::getZip);       // UC 12
             default -> Comparator.comparing(Contact::getFirstName);
         };
         contactList.stream().sorted(comp).forEach(System.out::println);
